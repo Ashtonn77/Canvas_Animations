3a53181be1fe3745c7167ecedb71acc9293f3ecf
ashton naidoo <ashtonrbay@gmail.com>
2020-04-17 22:47:04 +0200

    added main.js

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 import { voidInit, voidAnimate } from './followingVoid.js';
+import { gravityInit, gravityAnimate } from './gravity.js';
 
 let canvas = document.querySelector('canvas');
 canvas.width = innerWidth;
@@ -8,9 +9,53 @@ canvas.height = innerHeight;
 
 let c = canvas.getContext('2d');
 
+    
+    let voidStart = false;
+    let gravityStart = false;
+
+    //void button
+    // let followingVoidBtn = document.getElementById('void');
+    // followingVoidBtn.addEventListener('click', function(){
+    //     if(!voidStart){
+    //         voidInit(50);
+    //         voidAnimate();
+    //         voidStart = true;            
+    //     }
+    //     else voidInit();
+
+    // })
+
+    //gravity button
+    let gravityBtn = document.getElementById('gravity');
+    gravityBtn.addEventListener('click', function(){
+        if(!gravityStart){
+            gravityInit(100);
+            gravityAnimate();
+            gravityStart = true;
+        }
+        else gravityInit();
+    })
 
 
-voidInit();
-voidAnimate();
+
+
+
+let cntrls = document.getElementById('controls');
+
+let menuBtn = document.querySelector('.menu-btn');
+let menuOpen = false;
+
+menuBtn.addEventListener('click', function(){
+    if(!menuOpen){
+        menuBtn.classList.add('open');
+        cntrls.classList.add('open');
+        menuOpen = true;
+    }
+    else{
+        menuBtn.classList.remove('open');
+        cntrls.classList.remove('open');
+        menuOpen = false;
+    }
+})
 
 export { canvas, c };
